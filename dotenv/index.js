ea9f5c7dae52012838610a8d9b91bb0635f17d99
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2025-04-07 18:15:39 +0200

    @teeny-tiny/dotenv: Bug fix

--- dotenv/index.js
+++ dotenv/index.js
@@ -5,7 +5,7 @@ String.prototype.empty = function () {
 };
 
 module.exports = {
-  config: ({ path = "./.env", executeCode = false }) => Object.assign(process.env, fs.readFileSync(join(path), "utf8").split("\n").filter((line) => !line.startsWith("#") && (line.split("=").length > 1)).map((line) => line.trim().split(/\/\/|#/)[0].split("=")).reduce((data, accumulator) => ({
+  config: ({ path = "./.env", executeCode = false } = {}) => Object.assign(process.env, fs.readFileSync(join(path), "utf8").split("\n").filter((line) => !line.startsWith("#") && (line.split("=").length > 1)).map((line) => line.trim().split(/\/\/|#/)[0].split("=")).reduce((data, accumulator) => ({
     ...data,
     ...{
       [accumulator[0]]: JSON.parse(accumulator[1].trim()[(executeCode) ? "replaceAll" : "empty"](/<([^>]+)>/g, (_, expression) => eval(expression)))

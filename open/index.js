80d05920350cffb9d44c315f320a91354d384a82
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2025-04-19 17:59:38 +0200

    @teeny-tiny/open: Now a one-liner

--- open/index.js
+++ open/index.js
@@ -1,3 +1,1 @@
-module.exports = (url) => childProcess.exec(((process.platform === "win32") ? `start "` : ((process.platform === "darwin") ? `open "` : `xdg-open "`)) + url + "`", (process.platform === "win32") ? {
-  shell: "powershell.exe"
-} : {});
+module.exports = (url) => childProcess.exec(((process.platform === "win32") ? `start "` : ((process.platform === "darwin") ? `open "` : `xdg-open "`)) + url + "`", (process.platform === "win32") ? { shell: "powershell.exe" } : {});
